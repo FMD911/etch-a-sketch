483c12bb9e1dd1cecaba639336953afdf70432ad
Vupi <276366791+FMD911@users.noreply.github.com>
2026-05-21 00:43:01 +0200

    Fix working Etch-a-Sketch grid setup

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const container = document.querySelector("#container");
 
-for (let i = 0; i < 256; i++) {
+const GRID_SIZE = 16;
+const squareSize = 960 / GRID_SIZE;
+
+for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
   const square = document.createElement("div");
+
+  square.style.width = `${squareSize}px`;
+  square.style.height = `${squareSize}px`;
+
   container.appendChild(square);
 }
